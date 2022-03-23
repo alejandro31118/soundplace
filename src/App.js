b3f30060887e15de2105e657a69867ba1b4be27f
Alejandro Perez <alejandro31118@gmail.com>
2022-03-23 20:53:48 +0000

    Add functionality to LangSelector component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import LangSelector from './components/LangSelector/LangSelector';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation()
+
   return (
     <div className="App">
       <LangSelector />
+      {t('TEST')}
     </div>
   );
 }
--- src/components/LangSelector/LangSelector.js
+++ src/components/LangSelector/LangSelector.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+import cookies from 'js-cookie';
 import { languages } from './languages';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'flag-icon-css/css/flag-icons.min.css';
 
 const LangSelector = () => {
+    const currentLanguage = cookies.get('i18next') || 'en'
+    const currentLanguageCode = languages.find((lang) => lang.code === currentLanguage)
     const { t } = useTranslation()
 
     return (
       <div className="dropdown">
         <button className='btn btn-link dropdown-toggle' type='button' data-bs-toggle='dropdown'>
-          {t('LANG')}
+          <span className={`flag-icon flag-icon-${currentLanguageCode.country_code}`} />
         </button>
         <ul className='dropdown-menu'>
           {languages.map(language => (
             <li key={language.country_code}>
-              <a className='dropdown-item'>
-                <span className={`flag-icon flag-icon-${language.country_code} mx-2`} />
+              <a className='dropdown-item' onClick={() => { i18next.changeLanguage(language.code) }}>
+                <span className={`flag-icon flag-icon-${language.country_code} mx-2`} style={{ opacity: currentLanguage === language.code ? 0.5 : 1 }} />
                 {language.name}
               </a>
             </li>
