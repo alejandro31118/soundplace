19b0f9da819cc9fe6287587aa7f962c2f17c86e4
Alejandro Perez <alejandro31118@gmail.com>
2022-04-24 11:23:42 +0100

    Minor changes

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,20 +3,22 @@ import { Routes, Route } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import Home from '../Home/Home';
 import Library from '../Library/Library';
-import Trending from '../Trending/Trending';
+import Search from '../Search/Search';
 import Header from '../../components/Header/Header';
 import Player from '../../components/Player/Player';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Main = () => {
     const { t } = useTranslation()
 
     return (
-      <div className="">
+      <div className="bg-dark">
         <Header />
+
         <Routes>
             <Route path='/' element={ <Home /> } />
             <Route path='/library' element={ <Library /> } />
-            <Route path='/trending' element={ <Trending /> } />
+            <Route path='/search' element={ <Search /> } />
         </Routes>
 
         <Player />
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import axios from 'axios';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import SearchTracksResult from '../../components/SearchTracksResult/SearchTracksResult';
+import SearchArtistsResult from '../../components/SearchArtistsResult/SearchArtistsResult';
+import SearchPlaylistsResult from '../../components/SearchPlaylistsResult/SearchPlaylistsResult';
 import './Search.css';
 
 const Search = ({}) => {
@@ -15,6 +16,12 @@ const Search = ({}) => {
         <div className='flex-grow-1 my-2' style={{ overflowY: "auto" }}>
           <h4 className='text-light'>{ t('SONGS') }</h4>
           <SearchTracksResult query={ query }/>
+          
+          <h4 className='text-light mt-5'>{ t('ARTISTS') }</h4>
+          <SearchArtistsResult query={ query }/>
+
+          <h4 className='text-light mt-5'>{ t('PLAYLISTS') }</h4>
+          <SearchPlaylistsResult query={ query }/>
         </div>
       </div>
     );
