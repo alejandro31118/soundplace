b1b58b20ae42c1ee5b993010e8ef8e26e4e637aa
Alejandro Perez <alejandro31118@gmail.com>
2022-04-27 20:09:01 +0100

    Style login screen

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { loginEndpoint } from '../../context/authEndpoint';
 import LangSelector from '../../components/LangSelector/LangSelector';
 import Icon from '../../components/Icon/Icon';
-import spotifyLogo from './spotifyLogo.png';
 import bg from './NordMont.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
@@ -12,31 +11,30 @@ const Login = () => {
     const { t } = useTranslation()
 
     return (
-      <div>
-        <div className='login'>
-          <div className='d-block l-selector'>
+      <div className='row'>
+        <div className='d-none d-lg-block col-md-5'>
+          <img src={ bg } className='bg-image' alt='' />
+        </div>
+        <div className='col-12 col-md-7'>
+          <div className='col-12 p-3'>
             <LangSelector />
           </div>
-          <div className='d-flex align-items-center justify-content-center'>
-            <div className=''>
-              <div className='d-block text-center soundplace-div'>
-                <p><Icon name="logo" color="#3B4252" size="8rem" /></p>
-                <p className='title'>SoundPlace</p>
+          <div className='container main-div'>
+            <div className='mb-5 mt-5'>
+              <Icon name="logo" color="#3B4252" size="8rem" />
+              <h1 className='mb-5'><strong>SoundPlace</strong></h1>
+              <div className='container mt-5'>
+                <h2>{ t('LOGIN_MSG_1') }</h2>
+                <h3>{ t('LOGIN_MSG_2') }</h3>
               </div>
-              <div className='d-block text-center spotify-div'>
-                <p>{ t('COLLABORATION') }</p>
-                <img src={ spotifyLogo } className='spotify-logo mr-2' alt="" />
-                <span className='ml-2'>Spotify</span>
-              </div>
-              <div className='d-block text-center mt-5'>
-                <a href={ loginEndpoint } className='login-button text-center'> 
-                  { t('SIGN_IN') }
-                </a>
-              </div>
+            </div>
+            <div className='btn btn-color'>
+              <a href={ loginEndpoint } className='link'> 
+                { t('SIGN_IN') }
+              </a>
             </div>
           </div>
         </div>
-        <img src={ bg } className='bg-image' alt='' />
       </div>
     );
   };
