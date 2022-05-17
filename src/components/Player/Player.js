3fdf0eff1bf654e495f95d4ade4f997cffa1c67e
Alejandro Perez <alejandro31118@gmail.com>
2022-05-17 18:10:51 +0100

    Deployment changes

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 
